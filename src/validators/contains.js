9c6179ac73d83e9b5fcf014e98c3c612ea745542
Raymond Benc <me@raybenc.com>
2018-06-11 15:11:04 +0200

    Adding a dependency order during install.

--- src/validators/contains.js
+++ src/validators/contains.js
@@ -3,5 +3,16 @@ module.exports = function (data, reserved) {
     if (data === undefined) {
         return true;
     }
+    if (Array.isArray(data)) {
+        let pass = 0;
+        for (let key of data) {
+            if (reserved.includes(key)) {
+                pass++;
+            }
+        }
+
+        return (pass !== data.length);
+    }
+
     return !reserved.includes(data);
 };
